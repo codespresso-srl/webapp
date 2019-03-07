bbf21580122e87e4a38cc7d3ff30140270864ffd
Sorin Ilies <ilies.g.sorin@gmail.com>
2019-03-07 14:33:53 +0200

    added rate limit

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/", apiLimiter);
 app.use(helmet())
 app.disable('x-powered-by')
 
-
+// Serve css resources
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
